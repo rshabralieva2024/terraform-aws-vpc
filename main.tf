84a5e92ed150fde15005cc4290e564c88bea47b9
Aman Ullah <aman.ullah_contractor@gaintheory.com>
2024-05-11 00:08:49 +0500

    vpc output

--- main.tf
+++ main.tf
@@ -68,3 +68,11 @@ resource "aws_route_table_association" "private" {
 output "vpc_id" {
   value = aws_vpc.main.id
 }
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
